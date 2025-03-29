b4d1e79fb6dd5f8e9245adb509da7fce4a20a2f7
Aashish-Thapa <aashishthapa520@gmail.com>
2025-03-29 12:05:25 -0500

    some changes

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -14,43 +14,7 @@ function Header() {
                         <div className='col-xl-7 d-flex flex-column align-items-center align-items-md-start justify-content-center mt-5'>
                             <h2 className='fw-bold fs-3 text-center text-md-start'>Welcome to</h2>
                             <h1 className='text-capitalize fw-bold text-center text-md-start'>Nepali Spice</h1>
-                            <p className='mb-4 text-center text-md-start'>Welcome to Nepali Spice in London, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
-                            <div className='d-flex flex-column flex-sm-row'>
-                                <MenuBtn />
-                                <Link to='/contact'>
-                                    <button type='button' className='btn btn-outline-light btn-lg rounded-0 text-capitalize mx-2 shadow'>Contact us</button>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className='col-xl-5 d-none d-xl-block'></div>
-                    </div>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className='carousel-item2 vh-100'>
-                <Carousel.Caption className='h-100 pb-0'>
-                    <div className="row h-100">
-                        <div className='col-xl-7 d-flex flex-column align-items-center align-items-md-start justify-content-center mt-5'>
-                            <h2 className='fw-bold fs-3 text-center text-md-start'>Welcome to</h2>
-                            <h1 className='text-capitalize fw-bold text-center text-md-start'>Nepali Spice</h1>
-                            <p className='mb-4 text-center text-md-start'>Welcome to Nepali Spicein London, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
-                            <div className='d-flex flex-column flex-sm-row'>
-                                <MenuBtn />
-                                <Link to='/contact'>
-                                    <button type='button' className='btn btn-outline-light btn-lg rounded-0 text-capitalize mx-2 shadow'>Contact us</button>
-                                </Link>
-                            </div>
-                        </div>
-                        <div className='col-xl-5 d-none d-xl-block'></div>
-                    </div>
-                </Carousel.Caption>
-            </Carousel.Item>
-            <Carousel.Item className='carousel-item3 vh-100'>
-                <Carousel.Caption className='h-100 pb-0'>
-                    <div className="row h-100">
-                        <div className='col-xl-7 d-flex flex-column align-items-center align-items-md-start justify-content-center mt-5'>
-                            <h2 className='fw-bold fs-3 text-center text-md-start'>Welcome to</h2>
-                            <h1 className='text-capitalize fw-bold text-center text-md-start'>Nepali Spice</h1>
-                            <p className='mb-4 text-center text-md-start'>Welcome to Nepali Spice in Los Angeles, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
+                            <p className='mb-4 text-center text-md-start'>Welcome to Nepali Spice in Riverside, Ontario, California, the perfect experience for special people. Our fancy restaurant will bring your special moments to another level.</p>
                             <div className='d-flex flex-column flex-sm-row'>
                                 <MenuBtn />
                                 <Link to='/contact'>
